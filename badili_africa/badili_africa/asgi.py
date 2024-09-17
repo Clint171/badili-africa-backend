cc2195982757a81971c522cf9fb27ceb0502d76b
Clint171 <clintsimiyu004@gmail.com>
2024-09-17 08:01:56 +0300

    feat: added configurations

---
+++ badili_africa/badili_africa/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for badili_africa project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'badili_africa.settings')
+
+application = get_asgi_application()
