486d1f7ab782250a695de2ef14a5ead54fb03120
Clint171 <clintsimiyu004@gmail.com>
2024-09-19 11:19:59 +0300

    updated models

--- badili_africa/finance/models.py
+++ badili_africa/finance/models.py
@@ -31,24 +31,25 @@ class User(AbstractUser):
         return f"{self.first_name} {self.last_name}"
 
 class Project(models.Model):
-    project_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     description = models.TextField()
-    start_date = models.DateField()
-    end_date = models.DateField()
+    activities = models.TextField()
+    status = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.project_name
+        return self.name
 
 class Expense(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, default=None)
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE, default=None)
+    project_name = models.CharField(max_length=100)
     activity = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    ## Think through this
     receipt = models.FileField(upload_to="receipts/")
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    project_officer_id = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    project_officer = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.project.project_name} - {self.amount}"
+        return f"{self.project_id.name} - {self.amount}"
