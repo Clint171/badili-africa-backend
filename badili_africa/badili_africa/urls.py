179fe79ab3ce49cae5286995782f4201b4e174f4
Otero <oteronickbrian@gmail.com>
2024-10-01 00:14:37 +0300

    installed requirements.txt

--- badili_africa/badili_africa/urls.py
+++ badili_africa/badili_africa/urls.py
@@ -5,16 +5,24 @@ from rest_framework import routers
 from finance import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'projects', views.ProjectViewSet)
-router.register(r'expenses', views.ExpenseViewSet)
+router.register(r"users", views.UserViewSet)
+router.register(r"projects", views.ProjectViewSet)
+router.register(r"expenses", views.ExpenseViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/projects/<int:project_id>/expenses/', views.get_expenses_by_project, name='expenses-by-project'),
-    path('api/signup/', views.SignupView.as_view(), name='signup'),
-    path('api/login/', views.LoginView.as_view(), name='api_login'),
-    path('api/projects/<str:project_name>/update_status/', views.update_project_status, name='update_project_status'),
-    path('api/file/', views.upload_receipt_and_extract_data, name='upload_receipt'),
+    path("api/", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path(
+        "api/projects/<int:project_id>/expenses/",
+        views.get_expenses_by_project,
+        name="expenses-by-project",
+    ),
+    path("api/signup/", views.SignupView.as_view(), name="signup"),
+    path("api/login/", views.LoginView.as_view(), name="api_login"),
+    path(
+        "api/projects/<str:project_name>/update_status/",
+        views.update_project_status,
+        name="update_project_status",
+    ),
+    path("api/file/", views.upload_receipt_and_extract_data, name="upload_receipt"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
